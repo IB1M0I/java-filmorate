933d87184acbfa0ffc2e67d1e09d5c6fcbd78405
B1M0 <ChopMaksim@yandex.com>
2026-06-08 09:35:26 +0300

    Измерить класс User с использованим аннотаций

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,27 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import java.time.LocalDate;
+
+
+@Data
 public class Film {
+    Long id; //ID фильма
+
+    @NotBlank(message = "Название фильма не может быть пустым")
+    String name; //Название фильма
+
+    @Size(max = 200, message = "Описание фильма не может быть более 200 символов")
+    String description; //Описание фильма
+
+    LocalDate releaseDate; //Дата выпуска фильма
+
+    @Positive
+    Integer duration; //Продолжительность фильма
+
+
 }
