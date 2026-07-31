b2e0d5008b986cc8cf4de59cbce6896280f95c20
Nytik2002 <NytLocki@yandex.ru>
2026-07-31 17:17:21 +0500

    Исправление checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/dto/UpdateFilmRequest.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/dto/UpdateFilmRequest.java
@@ -47,5 +47,8 @@ public class UpdateFilmRequest {
         return genres != null && !genres.isEmpty();
     }
 
-    public boolean hasDirectors() { return directors != null && !directors.isEmpty(); }
+    public boolean hasDirectors()
+    {
+        return directors != null && !directors.isEmpty();
+    }
 }
