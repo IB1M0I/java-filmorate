d9699544e5655bb76c3d66c862d30cd32842f098
B1M0 <ChopMaksim@yandex.com>
2026-08-01 10:29:01 +0300

    Исправить ошибку CheckStyle

--- src/test/java/ru/yandex/practicum/filmorate/filmTest/FilmDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/filmTest/FilmDbStorageTest.java
@@ -23,7 +23,6 @@ import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
